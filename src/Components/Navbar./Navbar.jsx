d12a370297cd1ef4adfe810f86552e288133b705
Abhishek Bharti <abhishek@Abhisheks-MacBook-Air.local>
2023-03-06 14:02:17 +0530

    added links to components from navbar

--- src/Components/Navbar./Navbar.jsx
+++ src/Components/Navbar./Navbar.jsx
@@ -1,29 +1,62 @@
 import React from "react";
 import Toggle from "../Toggle/Toggle";
-import  './Navbar.css'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
+import "./Navbar.css";
+import Services from "../Services/Services.jsx";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
-    <div className="n-wrapper">
+    <div className="n-wrapper" id="Navbar">
       <div className="n-left">
         <div className="n-name">Abhishek</div>
-        <Toggle/>
+        <Toggle />
       </div>
       <div className="n-right">
         <div className="n-list">
-          <ul style={{listStyleType:"none"}}>
-            <li>Home</li>
-            <li>Services</li>
-            <li>Experience</li>
-            <li>Portfolio</li>
-            <li>Testimonial</li>
+          <ul style={{ listStyleType: "none" }}>
+            <Link
+              spy={true}
+              to="Navbar"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Home</li>
+            </Link>
+            <Link
+              spy={true}
+              to="Services"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Services</li>
+            </Link>
+            <Link
+              spy={true}
+              to="Experience"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Experience</li>
+            </Link>
+            <Link
+              spy={true}
+              to="Portfolio"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Portfolio</li>
+            </Link>
+            <Link
+              spy={true}
+              to="Testimonial"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Testimonial</li>
+            </Link>
           </ul>
         </div>
-        <button className="button n-button">
-           Contact
-        </button>
+        <button className="button n-button">Contact</button>
       </div>
     </div>
   );
